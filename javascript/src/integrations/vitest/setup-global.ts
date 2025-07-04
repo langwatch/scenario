67143efa2e2ac2566134016907e821c730839362
Alexander Forbes-Reed <git@alex.forbes.red>
2025-07-04 16:43:27 +0200

    use env vars instead of project injection

--- javascript/src/integrations/vitest/setup-global.ts
+++ javascript/src/integrations/vitest/setup-global.ts
@@ -1,17 +1,7 @@
-import type { TestProject } from 'vitest/node'
 import { generate } from 'xksuid';
 
-export default function setup(project: TestProject) {
+export default function setup() {
   const scenarioBatchRunId = `scenariobatchrun_${generate()}`;
 
-  // Inject the scenario batch run id into the environment
-  project.provide('scenarioBatchRunId', scenarioBatchRunId);
-  project.provide('reportingEnabled', true);
-}
-
-declare module 'vitest' {
-  export interface ProvidedContext {
-    scenarioBatchRunId: string;
-    reportingEnabled: boolean;
-  }
+  process.env.SCENARIO_BATCH_RUN_ID = scenarioBatchRunId;
 }
