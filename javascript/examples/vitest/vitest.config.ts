a6ddb5ff296bed330200bdd2dc2f36b20be0ee1a
Alexander Forbes-Reed <git@alex.forbes.red>
2025-07-04 16:59:31 +0200

    restore setup files

--- javascript/examples/vitest/vitest.config.ts
+++ javascript/examples/vitest/vitest.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from "vitest/config";
 export default withScenario(defineConfig({
   test: {
     testTimeout: 180_000, // 3 minutes
+    setupFiles: [
+      "dotenv/config",
+    ],
   },
 }));
